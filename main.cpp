9d8fa0139bd1d233f2ff5e5fbe180793147e73c5
m-kichik <kichik.mg@phystech.edu>
2023-12-28 17:53:28 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mf_3d.h"
 
-int main() {
-    auto mf = MagneticField("/home/rito4ka/dev/magnetic_field_BTR/Basic_PMS.txt");
+int main(int argc, char *argv[]) {
+    auto mf = MagneticField(argv[1]);
     auto field = mf.get_field(0.25, -0.15, 0.42);
     std::cout << field[0] << " " << field[1] << " " << field[2] << std::endl;
 
