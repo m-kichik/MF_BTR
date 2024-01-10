ffc5385be9b41cb850741df435e00bfb3e7041ce
m-kichik <kichik.mg@phystech.edu>
2024-01-10 12:55:56 +0300

    add some structure

--- mf_3d.py
+++ mf_3d.py
@@ -1,5 +1,42 @@
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 import mf_3d
 
+def py_parse_mf(mf_file:str):
+    with open(mf_file, 'r') as file:
+        content = file.read().strip().split('\n')
+        content  = np.array([line.strip().split() for line in content if line[0].isdigit()]).astype(float)
+
+    X  = content[:, 0]
+    Y  = content[:, 1]
+    Z  = content[:, 2]
+    Bx = content[:, 3]
+    By = content[:, 4]
+    Bz = content[:, 5]
+
+    fig = plt.figure()
+    fig = plt.figure(figsize=(12, 45))
+    ax = fig.add_subplot(projection='3d')
+    ax.set_box_aspect((7.0, 1.0, 3))
+    # ax.set_aspect('equalxy')
+    ax.scatter(X, Z, Bz, c=Bz, cmap='viridis', s=100)
+    plt.savefig('basic_pms_vis_X-Z-Bz.png')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', type=str, help='mf file')
+
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    mf = mf_3d.MagneticField('Basic_PMS.txt')
+    mf_file = parse_args().file
+
+    py_parse_mf(mf_file)
+
+    mf = mf_3d.MagneticField(mf_file)
+
+
     print(mf.get_field(0.1, 0.25, 0.333))
